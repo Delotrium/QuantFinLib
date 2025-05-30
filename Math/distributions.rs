9144b1358baeed372052fc3445c0e37bdf337b6c
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2025-05-30 23:42:07 +1000

    added skew and kurtosis

--- Math/distributions.rs
+++ Math/distributions.rs
@@ -13,3 +13,33 @@ pub fn gaussian_distr(mean: f64, sd: f64, x: f64) -> f64 {
     let exponent = -((x - mean) * (x - mean)) / (2.0 * sd * sd);
     coeff * f64::exp(exponent)
 }
+
+pub fn skew(numbers: &[f64]) -> f64
+{
+    let mean = crate::Math::basic_probability::mean(numbers);
+    let sd = crate::Math::basic_probability::standard_deviation_sample(numbers);
+    let coeff:f64 = 1.0/((numbers.len() as f64) * f64::powf(sd,3.0));
+    let mut skew:f64 =0f64;
+    let mut i = 1;
+    while i< numbers.len()
+    {
+        skew += f64::powf(numbers[i] - mean, 3.0);
+        i+=1;
+    }
+    skew * coeff
+}
+
+pub fn kurtosis(numbers: &[f64]) -> f64
+{
+    let mean = crate::Math::basic_probability::mean(numbers);
+    let sd = crate::Math::basic_probability::standard_deviation_sample(numbers);
+    let coeff:f64 = 1.0/((numbers.len() as f64) * f64::powf(sd,4.0));
+    let mut kurt:f64 =0f64;
+    let mut i = 1;
+    while i < numbers.len()
+    {
+        kurt += f64::powf(numbers[i]-mean, 4.0);
+        i+=1;
+    }
+    kurt * coeff - 3.0
+}
