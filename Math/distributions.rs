21b35aaab2f1e9a7685609ca01c577754f2bd573
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2025-05-31 11:52:28 +1000

    Update distributions.rs

--- Math/distributions.rs
+++ Math/distributions.rs
@@ -7,7 +7,7 @@ pub fn poisson(lambda:f64, x :u64)->f64
 
 pub fn gaussian_distr(mean: f64, sd: f64, x: f64) -> f64 {
     if sd <= 0.0 {
-        return f64::NAN; // or panic!("Standard deviation must be positive");
+        return f64::NAN;
     }
     let coeff = 1.0 / (sd * f64::sqrt(2.0 * PI));
     let exponent = -((x - mean) * (x - mean)) / (2.0 * sd * sd);
