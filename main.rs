ad4fd8380371b2646d531b2df4bc6e5ad78ad10e
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2025-06-02 14:58:23 +1000

    Update main.rs

--- main.rs
+++ main.rs
@@ -3,6 +3,6 @@
 
 fn main()
 {
-  println!("Estimated Options Price: ${}", qfl::finance::options::black_scholes_price(100f64, 110f64, 3f64, 0.04, 3f64, 0f64, true));
+  println!("Estimated Options Price: ${}", qfl::finance::options::black_scholes_price(100f64, 120f64, 3f64, 0.02, 3f64, 0f64, true));
   
 }
