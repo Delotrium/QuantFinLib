fb50b31fea60a0fc29e3d963f8f585aa64e05da6
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2025-11-13 13:54:07 +1000

    test case from CSV

--- main.rs
+++ main.rs
@@ -1,6 +1,14 @@
 
 fn main()
 {
-  let values = vec![1.0, 2.0, 3.0, 4.0, 6.0];
-  println!("SD: {}, AM: {}, Sum: {}", qfl::math::stats::standard_deviation_sample(&values), qfl::math::stats::mean(&values), qfl::math::stats::sum(&values));
+  qfl::engine::init_stocks::write_csv_to_bin("NVDA_stock_data.csv", "NVDA.bin").expect("ERROR LOADING CSV TO BIN");
+  let values = qfl::engine::init_stocks::bin_to_vec("NVDA.bin").expect("ERROR LOADING BIN TO VEC");
+  let prices: Vec<f64> = values.iter().map(|p| p.price).collect();
+  let prices: &[f64] = &prices;
+  println!("Mean: {}, SD: {}, GM: {}, AR: {}%, ALR: {}%", 
+    qfl::math::stats::mean(prices), 
+    qfl::math::stats::standard_deviation_sample(prices), 
+    qfl::math::stats::geometric_mean(prices),
+    qfl::finance::ratios::average_returns(prices),
+    qfl::finance::ratios::average_log_returns(prices));
 }
