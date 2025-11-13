0f1bf00ecb34ad0a8635f32b7569e82f77edb96a
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2025-11-13 13:53:56 +1000

    added bin to vc fn

--- engine/init_stocks.rs
+++ engine/init_stocks.rs
@@ -1,15 +1,15 @@
 use serde::{Deserialize, Serialize};
 use std::{error::Error, fs::File};
-use bincode::serialize_into;
+use bincode::{deserialize_from, serialize_into};
 
 #[derive(Serialize, Deserialize, Debug)]
-struct PricePoint 
+pub struct PricePoint 
 {
-    timestamp: u64,
-    price: f32,
+    pub timestamp: u64,
+    pub price: f64,
 }
 
-fn read_csv(path:&str) -> Result<Vec<PricePoint>, Box<dyn Error>>
+pub fn read_csv(path:&str) -> Result<Vec<PricePoint>, Box<dyn Error>>
 {
     let mut rdr = csv::Reader::from_path(path)?;
     let mut records = Vec::new();
@@ -20,18 +20,26 @@ fn read_csv(path:&str) -> Result<Vec<PricePoint>, Box<dyn Error>>
     Ok(records)
 }
 
-fn write_bincode(path: &str, data: &[PricePoint]) -> Result<(), Box <dyn Error>> 
+pub fn write_bincode(path: &str, data: &[PricePoint]) -> Result<(), Box <dyn Error>> 
 {
     let mut file = File::create(path)?;
     serialize_into(&mut file, data)?;
     Ok(())
 }
 
-fn write_csv_to_bin(ipath: &str, opath: &str) -> Result<(), Box <dyn Error>>
+pub fn write_csv_to_bin(ipath: &str, opath: &str) -> Result<(), Box <dyn Error>>
 {
     let data = read_csv(ipath)?;
     println!("Loaded {} records from {}...", data.len(), ipath);
     write_bincode(opath, &data)?;
     println!("Saved records to {}...", opath);
     Ok(())
+}
+
+pub fn bin_to_vec(ipath: &str) -> Result<Vec<PricePoint>, Box<dyn Error>>
+{
+    let file = File::open(ipath)?;
+    let prices: Vec<PricePoint> = deserialize_from(&file)?;
+    println!("Deserialized Bin to Vec");
+    Ok(prices)
 }
